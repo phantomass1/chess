8a324b89391786cd7c03825f83eaa2a944880400
phantomass1 <zimenko.vadim@gmail.com>
2022-06-30 13:59:25 +0300

    chess
    chess

---
+++ src/models/figures/King.ts
@@ -0,0 +1,26 @@
+import {Figure, FigureNames} from "./Figure";
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+// @ts-ignore
+import blackLogo from "../../assets/black-king.png";
+// @ts-ignore
+import whiteLogo from "../../assets/white-king.png";
+
+export class King extends Figure {
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell);
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+        this.name = FigureNames.KING;
+    }
+    canMove(target: Cell): boolean {
+        if(!super.canMove(target))
+            return false;
+        if(this.cell.isEmptyVertical(target))
+            return true;
+        if(this.cell.isEmptyHorizontal(target))
+            return true;
+        if(this.cell.isEmptyDiagonal(target))
+            return true;
+        return false
+    }
+}
